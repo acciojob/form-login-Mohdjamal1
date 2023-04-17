3141f3859c996232fbf2013f27d52316a9c81012
Mohd Jamal <79620177+Mohdjamal1@users.noreply.github.com>
2023-04-17 14:01:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	let first = document.querySelector("#form1")['fname'].value;
+  let last = document.querySelector("#form1")['lname'].value;
+ alert(`${first} ${last}`);
 }
